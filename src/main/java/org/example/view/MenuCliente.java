3fb97391d26f9c8519ba441477009995eda08eb4
PaniaguaSanty <paniaguasanty10@gmail.com>
2023-07-15 23:13:59 -0300

    Update MenuCliente.java

--- src/main/java/org/example/view/MenuCliente.java
+++ src/main/java/org/example/view/MenuCliente.java
@@ -155,10 +155,7 @@ public class MenuCliente {
         cliente.setTelefono(telefonoModificar);
     }
 
-    public void buscarCliente() {
-        
-    }
-
+ 
     public int getOption() {
         return option;
     }
